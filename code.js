d1a76254fac395d895801cccf46a95762ef7701d
Tiago Citrangulo da Silva <130724423+TiagoCitranguloDaSilva@users.noreply.github.com>
2023-08-08 00:07:27 -0300

    Melhorando o jogo

--- code.js
+++ code.js
@@ -118,92 +118,70 @@ function contagem(lugar){
     }
     if(lugar == maoAdversario){
         contAd.innerHTML = soma;
-        if(soma > 21){
-            alert('A casa estourou!')
-        }else if(soma == 21){
-            alert('BlackJack');
+        if(Number(contAd.innerHTML) > 21){
+            telaFinal()
+        }else if(Number(contAd.innerHTML) == 21){
+            telaFinal()
         }
     }else{
         contUser.innerHTML = soma;
-        if(soma > 21){
-            alert('Você estourou!')
-            stop();
-        }else if(soma == 21){
-            alert('BlackJack');
+        if(Number(contUser.innerHTML) > 21){
+            stop(true);
+            telaFinal()
+        }else if(Number(contUser.innerHTML) == 21){
+            stop(true);
+            telaFinal()
         }
     }
 }
 
-function stop(){
+function stop( notAd = false){
+    
     let main = document.getElementById('prin');
     main.style.pointerEvents = 'none';
-    setTimeout(adversario, 1500);
+    if(!notAd){
+        setTimeout(adversario, 1500);
+    }
 }
 
-function adversario(){
-    maoAdversario.children[0].classList.remove('escondida');
-    contagem(maoAdversario);
-    let cont = contAd.innerHTML;
-    let resto = 21 - cont;
-    let conts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for(let c = 0; c < cartasPossiveis.length; c++){
-        switch(cartasPossiveis[c][1]){
-            case 'A':
-                conts[0] += 1;
-                break;
-            case 2:
-                conts[1] += 1;
-                break;
-            case 3:
-                conts[2] += 1;
-                break;
-            case 4:
-                conts[3] += 1;
-                break;
-            case 5:
-                conts[4] += 1;
-                break;
-            case 6:
-                conts[5] += 1;
-                break;
-            case 7:
-                conts[6] += 1;
-                break;
-            case 8:
-                conts[7] += 1;
-                break;
-            case 9:
-                conts[8] += 1;
-                break;
-            case 10:
-                conts[9] += 1;
-                break;
-            case 'Q':
-                conts[10] += 1;
-                break;
-            case 'J':
-                conts[11] += 1;
-                break;
-            case 'K':
-                conts[12] += 1;
-                break;
-        }
-    }
-    if(resto >= 11){
-        setTimeout(add, 1500);
-    }
-
-    setTimeout(final, 1500);
-
+function telaFinal(){
+    setTimeout(()=>{
+        let tela = document.getElementById('telaFinal');
+        tela.style.display = 'flex';
+    }, 500);
     
 }
 
+function adversario(){
+    if(maoAdversario.children[0].classList[2] == 'escondida'){
+        maoAdversario.children[0].classList.remove('escondida');
+    }
+    contagem(maoAdversario);
+    
+    for(let c = 0; c < 5; c++){
+        if(Number(contAd.innerHTML) <= 16 && c < 5){
+            add();
+        }
+    } 
+    setTimeout(final, 1500);
+}
+
+
 function final(){
-    if(contAd.innerHTML <=21 && contUser.innerHTML <= 21){
-        if(contAd.innerHTML > contUser.innerHTML){
-            alert('O casa ganhou!');
-        }else if(contAd.innerHTML < contUser.innerHTML){
-            alert('Você ganhou!');
+
+    if(Number(contAd.innerHTML) <=21 && Number(contUser.innerHTML) <= 21){
+        if(Number(contAd.innerHTML) > Number(contUser.innerHTML)){
+            telaFinal()
+        }else if(Number(contAd.innerHTML) < Number(contUser.innerHTML)){
+            telaFinal()
+        }else{
+            telaFinal()
         }
     }
+}
+
+function novoJogo(){
+    setTimeout(()=>{
+        window.location.reload();
+    }, 500);
 }
